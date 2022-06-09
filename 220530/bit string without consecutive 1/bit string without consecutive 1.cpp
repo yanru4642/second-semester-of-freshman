7c2fce92101cc17350e7fc0cb1df23d86e3a1d0e
yanru4642 <yanru4642@gmail.com>
2022-06-09 21:53:49 +0800

    upload some practice projects

---
+++ 220530/bit string without consecutive 1/bit string without consecutive 1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int countStrings(int*& a, int*& b, int n);
+
+int main() {
+    int n;
+    int* a;
+    int* b;
+    int ans;
+    cout << "n-bit string without consecutive 1" << endl;
+    cout << "Enter n> ";
+    cin >> n;
+    a = new int[n];
+    b = new int[n];
+    ans = countStrings(a, b, n);
+    cout << n << "-bit: " << ans << endl;
+    return 0;
+}
+
+int countStrings(int*& a, int*& b, int n) {
+    a[0] = b[0] = 1;
+    for (int i = 1; i < n; i++)
+    {
+        a[i] = a[i - 1] + b[i - 1];
+        b[i] = a[i - 1];
+    }
+    return a[n - 1] + b[n - 1];
+}
---
+++ 220530/polymorphism practice 1/polymorphism practice 1.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+	virtual void speak() = 0;
+};
+class Cat :public Animal {
+public:
+	int getCatAge() { return 2; }
+	void speak() {
+		cout << "Cat speaking~" << endl;
+	}
+};
+class Dog :public Animal {
+public:
+	int getDogAge() { return 5; }
+	void speak() {
+		cout << "Dog speaking~" << endl;
+	}
+};
+
+void doSpeak(Animal* pAnimail) {
+	pAnimail->speak();
+	if (Cat* pCat = dynamic_cast<Cat*>(pAnimail)) {
+		cout << "Cat's age: " << pCat->getCatAge() << endl;
+	}
+	if (Dog* pDog = dynamic_cast<Dog*>(pAnimail)) {
+		cout << "Dog's age: " << pDog->getDogAge() << endl;
+	}
+}
+
+int main() {
+	Cat cat;
+	doSpeak(&cat);
+	Dog dog;
+	doSpeak(&dog);
+}
